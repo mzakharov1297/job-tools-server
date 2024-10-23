01fb68e523858bcbbd7644829c65b17157fc1f9d
Nfv <fraggedbynfv@gmail.com>
2024-10-23 23:44:33 +0300

    main: init commit

---
+++ src/main/java/com/job/tools/JobToolsApplication.java
@@ -0,0 +1,12 @@
+package com.job.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobToolsApplication.class, args);
+    }
+}
---
+++ src/main/java/com/job/tools/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.job.tools.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/test")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("Hello world");
+    }
+}
